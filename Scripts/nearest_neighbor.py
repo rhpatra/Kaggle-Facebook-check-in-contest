3c4e2adef0c65a7fc39c00cdc8ab666597ed095a
abhishek kadian <abhishekkadiyan@gmail.com>
2016-06-05 19:44:52 +0530

    Modified the iloc call to improve speed.
    Changed the iloc call so that the method is called only once with the
    list of all indices required. This showed significant improvement
    compared to when the iloc was being called multiple times.

--- Scripts/nearest_neighbor.py
+++ Scripts/nearest_neighbor.py
@@ -9,9 +9,14 @@ def nn(trainfile_path, testfile_path):
     test = pd.read_csv(testfile_path)
     tree = KDTree(train[['x', 'y']])
     _, ind = tree.query(test[['x', 'y']], k=3)
-    temp = [train.iloc[x].place_id.values for x in ind]
-    temp = [" ".join((str(y) for y in x)) for x in temp]
-    test['place_id'] = temp
+    temp = [y for x in ind for y in x]
+    temp_ind = [i for i, x in enumerate(ind) for y in x]
+    temp = train.iloc[temp].place_id.values
+    preds = [[] for x in range(test.shape[0])]
+    for a, b in zip(temp_ind, temp):
+        preds[a].append(str(b))
+    preds = [" ".join(x) for x in preds]
+    test['place_id'] = preds
     test[['row_id', 'place_id']].to_csv('submission_NN.gz', index=False, compression='gzip')
 
 
